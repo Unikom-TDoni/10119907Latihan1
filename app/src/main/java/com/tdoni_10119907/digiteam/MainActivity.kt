27981e7b1e1b097ffbeb911acc8061d30adc0e33
Theodorus Doni <theodorusdoni4@gmail.com>
2022-04-17 11:28:14 +0700

    Initial Commit

---
+++ app/src/main/java/com/tdoni_10119907/digiteam/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tdoni_10119907.digiteam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * T DONI
+ * 10119907
+ * IF 10K
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
